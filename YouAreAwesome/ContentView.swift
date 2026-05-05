9d3985607219ac2f04aa4419e9acf12b9334b7f8
Tyler Thames <tylerthames@bellsouth.net>
2026-05-04 20:12:06 -0500

    Lesson 1.7 Adding Images

--- YouAreAwesome/ContentView.swift
+++ YouAreAwesome/ContentView.swift
@@ -9,44 +9,36 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var message = ""
-    @State private var imageString = ""
+    @State private var imageName = ""
     var body: some View {
         
         VStack {
             Spacer()
             
-            Image(systemName: imageString)
+            Image(imageName)
                 .resizable()
                 .scaledToFit()
-                .foregroundStyle(.orange)
+                .clipShape(RoundedRectangle(cornerRadius: 30))
+                .shadow(radius: 30)
             
             Text(message)
                 .font(.largeTitle)
-                .fontWeight(.ultraLight)
-                .foregroundStyle(.purple)
+                .fontWeight(.heavy)
+                .foregroundStyle(.red)
             
             Spacer()
             
             Button("Press Me!") {
                 let message1 = "Your Are Awesome!"
                 let message2 = "You Are Great!"
-                let imageString1 = "hand.thumbsup"
-                let imageString2 = "sun.max.fill"
                 
-//                if message == message1 {
-//                    message = message2
-//                    imageString = imageString1
-//                } else {
-//                    message = message1
-//                    imageString = imageString2
-//                }
                 message = (message == message1 ? message2 : message1)
-                imageString = (imageString == imageString1 ? imageString2 : imageString1)
+                imageName = (imageName == "image0" ? "image1" : "image0")
             }
             .buttonStyle(.borderedProminent)
             .buttonBorderShape(.roundedRectangle)
             .font(.title2)
-            .tint(.orange)
+            
             
         }
         .padding()
