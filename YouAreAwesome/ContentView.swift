01ebe23def4588379f6ab34030ea164e8f527aef
Tyler Thames <tylerthames@bellsouth.net>
2026-05-04 19:49:53 -0500

    Lesson 1.6 Switch Case and Ternary Operator

--- YouAreAwesome/ContentView.swift
+++ YouAreAwesome/ContentView.swift
@@ -33,13 +33,15 @@ struct ContentView: View {
                 let imageString1 = "hand.thumbsup"
                 let imageString2 = "sun.max.fill"
                 
-                if message == message1 {
-                    message = message2
-                    imageString = imageString1
-                } else {
-                    message = message1
-                    imageString = imageString2
-                }
+//                if message == message1 {
+//                    message = message2
+//                    imageString = imageString1
+//                } else {
+//                    message = message1
+//                    imageString = imageString2
+//                }
+                message = (message == message1 ? message2 : message1)
+                imageString = (imageString == imageString1 ? imageString2 : imageString1)
             }
             .buttonStyle(.borderedProminent)
             .buttonBorderShape(.roundedRectangle)
