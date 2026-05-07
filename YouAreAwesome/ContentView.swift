fac850a1b4574a830a7f27a789c7f573162b5b51
Tyler Thames <tylerthames@bellsouth.net>
2026-05-06 19:17:07 -0500

    Lesson 1. 8 Intro to Types - Cycle through images

--- YouAreAwesome/ContentView.swift
+++ YouAreAwesome/ContentView.swift
@@ -10,6 +10,7 @@ import SwiftUI
 struct ContentView: View {
     @State private var message = ""
     @State private var imageName = ""
+    @State private var imageNumber = 0
     var body: some View {
         
         VStack {
@@ -28,12 +29,19 @@ struct ContentView: View {
             
             Spacer()
             
-            Button("Press Me!") {
+            Button("Show Message") {
                 let message1 = "Your Are Awesome!"
                 let message2 = "You Are Great!"
                 
                 message = (message == message1 ? message2 : message1)
-                imageName = (imageName == "image0" ? "image1" : "image0")
+                
+                imageName = "image\(imageNumber)"
+
+                imageNumber += 1
+                
+                if imageNumber > 9 {
+                    imageNumber = 0
+                }
             }
             .buttonStyle(.borderedProminent)
             .buttonBorderShape(.roundedRectangle)
