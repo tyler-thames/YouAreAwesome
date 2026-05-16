dd414270d0d35a7dac33878a169f57065d0436e7
Tyler Thames <tylerthames@bellsouth.net>
2026-05-16 13:54:48 -0500

    1.12 Added while and repeat loops

--- YouAreAwesome/ContentView.swift
+++ YouAreAwesome/ContentView.swift
@@ -10,8 +10,8 @@ import SwiftUI
 struct ContentView: View {
     @State private var message = ""
     @State private var imageName = ""
-    @State private var imageNumber = 0
-    @State private var messageNumber = 0
+    @State private var lastMessageNumber = -1
+    @State private var lastImageNumber = -1
     var body: some View {
         
         VStack {
@@ -42,9 +42,19 @@ struct ContentView: View {
                                 "You Make Me Smile!",
                 ]
                 
-                message = messages[Int.random(in: 0...messages.count-1)]
+                var messageNumber: Int
+               repeat {
+                    messageNumber = Int.random(in: 0...messages.count - 1)
+                } while messageNumber == lastMessageNumber
+                message = messages[messageNumber]
+                lastMessageNumber = messageNumber
                 
-                imageName = "image\(Int.random(in: 0...9))"
+                var imageNumber: Int
+                repeat {
+                    imageNumber = Int.random(in: 0...9)
+                } while imageNumber == lastImageNumber
+                imageName = "image\(imageNumber)"
+                lastImageNumber = imageNumber
                 
             }
             .buttonStyle(.borderedProminent)
